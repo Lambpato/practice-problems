e003d45826a9bd702cff7673217a5cc76a4d7904
Lambpato <lambpato@gmail.com>
2025-04-13 20:39:14 -0700

    Completed leetcode problem 445
    - Done in typescript
    - 5ms runtime
    - 61.46 Memory used

--- add-two-numbers-II/README.md
+++ add-two-numbers-II/README.md
@@ -6,7 +6,7 @@ You may assume the two numbers do not contain any leading zero, except the numbe
 
 Example 1:
 
-https://assets.leetcode.com/uploads/2021/04/09/sumii-linked-list.jpg
+![alt text](image.png)
 
 Input: l1 = [7,2,4,3], l2 = [5,6,4]
 Output: [7,8,0,7]
--- add-two-numbers-II/addTwoNumbersII.ts
+++ add-two-numbers-II/addTwoNumbersII.ts
@@ -11,5 +11,29 @@
  */
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-    let remainder = 0;
-};
+    const stack1: number[] = [];
+    const stack2: number[] = [];
+
+    while (l1) {
+        stack1.push(l1.val);
+        l1 = l1.next;
+    }
+
+    while (l2) {
+        stack2.push(l2.val);
+        l2 = l2.next;
+    }
+
+    let carry = 0;
+    let result: ListNode | null = null;
+
+    while (stack1.length > 0 || stack2.length > 0 || carry > 0) {
+        const sum = (stack1.pop() ?? 0) + (stack2.pop() ?? 0) + carry;
+        carry = Math.floor(sum / 10);
+        const newNode = new ListNode(sum % 10);
+        newNode.next = result;
+        result = newNode;
+    }
+
+    return result;
+}
